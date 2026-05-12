66525b7a2c20231b5f689a7babdf3dd383b97a37
lucia8307 <luciabigailchavezsosa@gmail.com>
2026-05-12 18:50:58 -0300

    Configura la base de datos

--- back/backend/settings.py
+++ back/backend/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'simulador_db',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
