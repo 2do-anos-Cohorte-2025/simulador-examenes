b83282c8cf4c73b16a8faaa4e610284a02c22f94
CristianPasquevich <cristianpasquevich@gmail.com>
2026-05-30 21:29:29 -0300

    fix: reactive forms y validaciones dashboard profesor

--- front/proyecto/src/app/views/dashboard/dashboard.component.ts
+++ front/proyecto/src/app/views/dashboard/dashboard.component.ts
@@ -1,13 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ExamFormComponent } from '../../components/exam-form/exam-form.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ExamFormComponent],
+  imports: [CommonModule,ExamFormComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  mostrarFormulario = false;
+  mostrarExamenes = false;
+  mostrarResultados = false;
+
+  mostrarCrearExamen() {
+    this.mostrarFormulario = true;
+    this.mostrarExamenes = false;
+    this.mostrarResultados = false;
+  }
+
+  mostrarListaExamenes() {
+
+  this.mostrarFormulario = false;
+  this.mostrarExamenes = true;
+  this.mostrarResultados = false;
+  }
+
+  mostrarResultadosExamenes() {
+
+  this.mostrarFormulario = false;
+  this.mostrarExamenes = false;
+  this.mostrarResultados = true;
+  }
 
 }
