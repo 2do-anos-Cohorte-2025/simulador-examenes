c41a3a98a7414b4d96e5a43339e6c9eed957a964
Quiroga Leandro <quirogaleandromanuel@gmail.com>
2026-06-01 14:56:34 -0300

    Agrego modelos restantes

--- back/api/admin.py
+++ back/api/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
-from .models import TestConnection
+from django.contrib.auth.admin import UserAdmin
 
+from .models import (
+    Usuario,
+    Profesor,
+    Examen,
+    Pregunta,
+    Opcion,
+    IntentoExamen,
+    RespuestaUsuario,
+    TestConnection
+)
+
+# Registrar CustomUser
+#admin.site.register(CustomUser, UserAdmin)
+
+# Registrar modelos restantes
+admin.site.register(Usuario)
+admin.site.register(Profesor)
+admin.site.register(Examen)
+admin.site.register(Pregunta)
+admin.site.register(Opcion)
+admin.site.register(IntentoExamen)
+admin.site.register(RespuestaUsuario)
 admin.site.register(TestConnection)
